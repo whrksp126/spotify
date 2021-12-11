0759add9f0319b8a7835e21b242d8773c6d46e8a
whrksp126 <whrksp126@naver.com>
2021-12-11 19:47:02 +0900

    paly error

--- components/Center.js
+++ components/Center.js
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from 'lodash';
 import { useRecoilState, useRecoilValue } from "recoil";
@@ -36,9 +36,10 @@ function Center() {
 
   console.log(playlist)
   return (
-    <div className="flex-grow ">
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide ">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
+        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white" 
+          onClick={signOut}>
           <img className="rounded-full w-10 h-10"src={session?.user.image} alt="" />
           <h2>{session?.user.name}</h2>
           <ChevronDownIcon className="h-5 w-5" />
--- components/Songs.js
+++ components/Songs.js
@@ -1,7 +1,14 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import Song from "./Song";
 function Songs() {
+
+  const playlist  = useRecoilValue(playlistState);
   return (
-    <div>
-      <h1>Songs</h1>
+    <div className="px-8 flex flex-col space-y-1 pb-28 text-white">
+      {playlist?.tracks.items.map((track, i)=>(
+        <Song key={track.track.id} track={track} order={i}/>
+      ))}
     </div>
   )
 }
